a5b27d3ddd8183f0a1d8f3a852fc2bbfd0d67e03
Igor Konobas <kois@ukr.net>
2018-12-05 21:05:46 +0200

    Unit-tested password managing tools

---
+++ src/test/com/javacourse/utils/PasswordGeneratorTest.java
@@ -0,0 +1,59 @@
+package com.javacourse.utils;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PasswordGeneratorTest {
+
+    private PasswordGenerator generator;
+
+    @Before
+    public void setUp() throws Exception {
+        generator = new PasswordGenerator.PasswordGeneratorBuilder()
+                .useDigits(true)
+                .useLower(true)
+                .useUpper(true)
+                .usePunctuation(false)
+                .build();
+    }
+
+    @Test
+    public void generate_zeroLength_returnsEmptyString() {
+        String expected = "";
+        String actual = generator.generate(0);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void generate_negativeLength_returnsEmptyString() {
+        String expected = "";
+        String actual = generator.generate(-1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void generate_fixedPositiveLength_returnsResultOfExactLength() {
+        int expected = 10;
+        int actual = generator.generate(10).length();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void generate_buildGeneratorWithoutPunctuationCharacterCategory_returnsStringWithoutPunctuationSymbols(){
+        long expected = 0;
+        long actual = generator.generate(100)
+                .chars()
+                .filter(x->"!@#$%&*()_+-=[]|,./?><".contains(String.valueOf((char)x)))
+                .count();
+        assertEquals(expected, actual);
+    }
+
+    @Test(timeout = 100)
+    public void generate_bigLength_runsUnder100Milisqconds(){
+        final int BIG_LENGTH = 1000;
+        generator.generate(BIG_LENGTH);
+    }
+}
---
+++ src/test/com/javacourse/utils/PasswordManagerTest.java
@@ -0,0 +1,63 @@
+package com.javacourse.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PasswordManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void hash_generateSHA256Hex_outputLengthIs64() {
+        String password = "my_strong_password123";
+        String salt = "my_salt123";
+
+        int expected = 64;
+        int actual = PasswordManager.hash(password, salt).length();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void hash_emptyInput_correctOutput() {
+        String password = "";
+        String salt = "";
+
+        int expected = 64;
+        int actual = PasswordManager.hash(password, salt).length();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void hash_nullInput_correctOutput() {
+        String password = null;
+        String salt = null;
+
+        int expected = 64;
+        int actual = PasswordManager.hash(password, salt).length();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void generateRandomPassword_noParams_DefaultLength10() {
+        int expected = 10;
+        String actual = PasswordManager.generateRandomPassword();
+        assertEquals(expected, actual.length());
+    }
+
+    @Test
+    public void isExpectedPassword() {
+        String password = "my_strong_password";
+        String salt = "my_salt";
+
+        String expectedHash = PasswordManager.hash(password, salt);
+
+        assertTrue(PasswordManager.isExpectedPassword(password, salt, expectedHash));
+    }
+}
