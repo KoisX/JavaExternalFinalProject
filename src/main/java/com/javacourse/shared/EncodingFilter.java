a40bc0bde89a192476e79b8905b1534ebd56c029
Igor Konobas <kois@ukr.net>
2018-12-17 20:02:59 +0200

    Implemented encoding filter

---
+++ src/main/java/com/javacourse/shared/EncodingFilter.java
@@ -0,0 +1,31 @@
+package com.javacourse.shared;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import java.io.IOException;
+
+@WebFilter(filterName = "EncodingFilter",
+           urlPatterns = {"/*"},
+           initParams = @WebInitParam(name="requestEncoding", value = "UTF-8"))
+public class EncodingFilter implements Filter {
+
+    private String encoding;
+
+    public void init(FilterConfig config) throws ServletException {
+        encoding = config.getInitParameter("requestEncoding");
+        if(encoding==null)
+            encoding = "UTF-8";
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        if(req.getCharacterEncoding()==null)
+            req.setCharacterEncoding(encoding);
+
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setCharacterEncoding(encoding);
+
+        chain.doFilter(req, resp);
+    }
+
+}
