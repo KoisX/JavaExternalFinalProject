34a6b9a564a18e981bcb758a69a2cd669c9a8371
Igor Konobas <kois@ukr.net>
2018-12-25 20:40:56 +0200

    Refactored Create command

--- src/main/java/com/javacourse/test/topic/commands/AddTopicCommand.java
+++ src/main/java/com/javacourse/test/topic/commands/AddTopicCommand.java
@@ -16,39 +16,28 @@ import java.util.Set;
 
 public class AddTopicCommand implements Command {
 
+    private String lang;
     private static final String NAME_PARAM = "name";
     private static final String LANG_PARAM = "lang";
     private static final String ERROR_REQUEST_MESSAGE = "error";
 
     @Override
     public WebPage execute(HttpServletRequest request) {
-        WebPage webPage = WebPage.TOPICS_ACTION;
-
-        TopicService topicService = new TopicService();
         Topic topic = constructTopic(request);
+        lang = (String)request.getSession().getAttribute(LANG_PARAM);
 
-        String lang = (String)request.getSession().getAttribute(LANG_PARAM);
-        Validator validator = BeanValidatorConfig.getValidator(lang);
-        Set<ConstraintViolation<Topic>> violations = validator.validate(topic);
+        //validating model and getting violations if sth is wrong
+        Set<ConstraintViolation<Topic>> violations = BeanValidatorConfig
+                .getValidator(lang)
+                .validate(topic);
 
+        //set error message if model is not valid
         if(!violations.isEmpty()){
             request.setAttribute(ERROR_REQUEST_MESSAGE, violations.iterator().next().getMessage());
             return WebPage.TOPICS_ADMIN_CREATE;
         }
 
-        //  А тепер заміни цей весь кусок на tryInsert
-
-        /*webPage = WebPage.TOPICS_ACTION;
-        try {
-            if(topicService.create(topic)){
-                webPage = WebPage.TOPICS_ACTION.setDoRedirect(true);
-            }
-        } catch (SQLException | UnsuccessfulQueryException e) {
-            ResourceBundle resourceBundle = ResourceBundleConfig.getResourceBundle(lang);
-            request.setAttribute(ERROR_REQUEST_MESSAGE, resourceBundle.getString("msg.creationUnsuccessful"));
-            webPage = WebPage.TOPICS_ADMIN_CREATE;
-        }*/
-        return webPage;
+        return getPageDependingOnWhetherInsertIsSuccessful(request, topic);
     }
 
     private Topic constructTopic(HttpServletRequest request) {
@@ -57,7 +46,7 @@ public class AddTopicCommand implements Command {
         return topic;
     }
 
-    private WebPage tryInsert(HttpServletRequest request, Topic topic){
+    private WebPage getPageDependingOnWhetherInsertIsSuccessful(HttpServletRequest request, Topic topic){
         WebPage webPage = WebPage.TOPICS_ACTION;
         TopicService topicService = new TopicService();
         try {
