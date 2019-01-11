5407eddd23ce15ad94e20023adc6d8b18381e092
Igor Konobas <kois@ukr.net>
2019-01-11 23:22:30 +0200

    Fixed form validation js script and starting working on rules i18n

--- src/main/webapp/scripts/validateHtmlForm.js
+++ src/main/webapp/scripts/validateHtmlForm.js
@@ -1,9 +1,10 @@
 /*Checking if at least one checkbox is checked*/
 $(document).ready(function () {
     $('#checkBtn').click(function() {
+        let checkboxes = $("input[type=checkbox]").length;
         let checked = $("input[type=checkbox]:checked").length;
 
-        if(!checked) {
+        if(!checked && checkboxes>0) {
             alert("You must check at least one checkbox in each checkbox group!");
             return false;
         }
